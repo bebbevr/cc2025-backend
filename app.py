60187a856e2a57dd74120c4944833b473a2ab248
Bebbe <bebbe.vr@gmail.com>
2025-03-11 14:19:13 +0200

    app.py update

--- app.py
+++ app.py
@@ -15,4 +15,4 @@ def get_sentiment():
     return {'input_data': input_data, 'message': 'hello'}
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="5000", debug=True)
+    app.run(host="0.0.0.0", port="8080", debug=False)
