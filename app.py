5963f756a14841c50d0912054609f2b2b722d3aa
Bebbe <bebbe.vr@gmail.com>
2025-03-11 14:28:03 +0200

    version3.3

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     print('hello')
-    return "<p>Hello world from cc backend</>"
+    return "<p>Hello world version 3.3</>"
 
 @app.route('/sentiment', methods=['POST'])
 def get_sentiment():
